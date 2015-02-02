de56b0e45e05501632049a4ac951cb6de5f5e675
E Brent Nelson <bnelson@hirevue.com>
2015-02-02 14:37:11 -0700

    General clean up.

--- examples/app.js
+++ examples/app.js
@@ -2,36 +2,36 @@ var React         = require('react');
 var HvReactAgenda = require('hv-react-agenda');
 
 var now = new Date(2015, 5, 24);
-var itemClasses = [
+var items = [
   {
-    itemName      : 'Snorlax, I choose you!',
+    name          : 'Snorlax, I choose you!',
     startDateTime : new Date(now.getFullYear(), now.getMonth(), now.getDate()-7, 8, 24),
     endDateTime   : new Date(now.getFullYear(), now.getMonth(), now.getDate()-7, 8, 49),
-    classNames    : 'snorlax'
+    classes       : 'snorlax'
   },
   {
-    itemName      : '\'Puff, I choose you!',
+    name          : '\'Puff, I choose you!',
     startDateTime : new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 12, 56),
     endDateTime   : new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 20, 14),
-    classNames    : 'jigglypuff'
+    classes       : 'jigglypuff'
   },
   {
-    itemName      : 'Select this time',
-    startDateTime : new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 16, 56),
-    endDateTime   : new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 20, 14),
-    classNames    : 'outline'
+    name          : 'Select this time',
+    startDateTime : new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 12, 56),
+    endDateTime   : new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 16, 56),
+    classes       : 'outline'
   },
   {
-    itemName      : 'Pikachu, I choose you!',
+    name          : 'Pikachu, I choose you!',
     startDateTime : new Date(now.getFullYear(), now.getMonth(), now.getDate()+3, 9, 35),
     endDateTime   : new Date(now.getFullYear(), now.getMonth(), now.getDate()+3, 11, 49),
-    classNames    : 'pikachu'
+    classes       : 'pikachu'
   },
   {
-    itemName      : 'Charizard, I choose you!',
+    name          : 'Charizard, I choose you!',
     startDateTime : new Date(now.getFullYear(), now.getMonth(), now.getDate()+7, 9, 14),
     endDateTime   : new Date(now.getFullYear(), now.getMonth(), now.getDate()+7, 17),
-    classNames    : 'charizard'
+    classes       : 'charizard'
   }
 ];
 
@@ -40,7 +40,7 @@ var App = React.createClass({
   render: function() {
     return (
       <div>
-        <HvReactAgenda startDate={now} startAtTime={7} itemClasses={itemClasses} />
+        <HvReactAgenda startDate={now} startAtTime={7} items={items} />
       </div>
     )
   }
--- modules/HvReactAgenda.js
+++ modules/HvReactAgenda.js
@@ -1,17 +1,17 @@
 var moment    = require('moment');
-var React     = require('react');
+var React     = require('react/addons');
 var PropTypes = React.PropTypes;
 
 var DEFAULT_ITEM = {
-  itemName    : '',
-  itemKeys    : [],
-  itemClasses : ''
+  id       : null,
+  name     : '',
+  classes  : '',
+  cellRefs : [],
+  firstRef : null,
+  lastRef  : null
 };
 
-var DEFAULT_ITEM_OVERLAY_STYLE = {
-  display: 'none'
-};
-
+/*
 function addDomClass(domNode, className) {
   removeDomClass(domNode, className); // dedupe
   domNode.className += ' ' + className;
@@ -21,6 +21,7 @@ function removeDomClass(domNode, className) {
   var classNames = domNode.className.replace(new RegExp(' ' + className, 'g'), '');
   domNode.className = classNames;
 }
+*/
 
 var HvReactAgenda = React.createClass({
 
@@ -30,11 +31,11 @@ var HvReactAgenda = React.createClass({
     startAtTime     : PropTypes.number.isRequired,
     rowsPerHour     : PropTypes.oneOf([1,2,3,4]).isRequired,
     numberOfDays    : PropTypes.oneOf([1,2,3,4,5,6,7]).isRequired,
-    itemClasses     : PropTypes.arrayOf(PropTypes.shape({
-      itemName      : PropTypes.string,
-      startDateTime : PropTypes.instanceOf(Date),
-      endDateTime   : PropTypes.instanceOf(Date),
-      classNames    : PropTypes.string
+    items           : PropTypes.arrayOf(PropTypes.shape({
+      name          : PropTypes.string,
+      startDateTime : PropTypes.instanceOf(Date).isRequired,
+      endDateTime   : PropTypes.instanceOf(Date).isRequired,
+      classes       : PropTypes.string
     })),
     onItemSelect    : PropTypes.func
   },
@@ -50,9 +51,10 @@ var HvReactAgenda = React.createClass({
 
   getInitialState: function() {
     return {
-      date: moment(),
-      itemClasses: {},
-      itemOverlayStyles: {}
+      date              : moment(),
+      items             : {},
+      itemOverlayStyles : {},
+      hoveredItemId     : ''
     }
   },
 
@@ -69,14 +71,14 @@ var HvReactAgenda = React.createClass({
       this.setState({date: moment(this.props.startDate)});
     }
 
-    if (this.props.itemClasses) {
-      this.setState({itemClasses: this.mapItemClasses(this.props.itemClasses)});
+    if (this.props.items) {
+      this.setState({items: this.mapItems(this.props.items)});
     }
   },
 
   componentWillReceiveProps: function() {
-    if (this.props.itemClasses) {
-      this.setState({itemClasses: this.mapItemClasses(this.props.itemClasses)});
+    if (this.props.items) {
+      this.setState({items: this.mapItems(this.props.items)});
     }
   },
 
@@ -88,40 +90,41 @@ var HvReactAgenda = React.createClass({
     this.setState({date: this.state.date.subtract(this.props.numberOfDays, 'days')});
   },
 
-  mapItemClasses: function(itemClassesArray) {
-    var itemClassesMap = {};
-    itemClassesArray.forEach(function(itemClass) {
-      var interval = (60/this.props.rowsPerHour);
-      var offsetMinutes = itemClass.startDateTime.getMinutes() % interval;
-      var start = moment(itemClass.startDateTime).subtract(offsetMinutes, "minutes").toDate();
-      var end   = itemClass.endDateTime;
-      var d     = moment.duration(moment(end).diff(moment(start)));
-      var rows  = Math.ceil(d.asHours()/(interval/60));
+  mapItems: function(itemsArray) {
+    var itemsMap = {};
+    itemsArray.forEach(function(item) {
+      var interval      = (60/this.props.rowsPerHour);
+      var offsetMinutes = item.startDateTime.getMinutes() % interval;
+      var start         = moment(item.startDateTime).subtract(offsetMinutes, "minutes").toDate();
+      var end           = item.endDateTime;
+      var duration      = moment.duration(moment(end).diff(moment(start)));
+      var rows          = Math.ceil(duration.asHours()/(interval/60));
 
-      var itemKeys = [];
+      var cellRefs = [];
       for (var i = 0; i < rows; i++) {
-        var key = moment(start).add(i*interval, 'minutes').format('YYYY-MM-DDTHH:mm:ss');
-        itemKeys.push(key);
+        var ref = moment(start).add(i*interval, 'minutes').format('YYYY-MM-DDTHH:mm:ss');
+        cellRefs.push(ref);
       }
 
-      var defaultStyles = {};
-      itemKeys.forEach(function(key) {
-        itemClassesMap[key] = {
-          itemName    : itemClass.itemName,
-          itemKeys    : itemKeys,
-          itemClasses : (itemClassesMap[key]) ? (itemClassesMap[key].itemClasses + ' ' + itemClass.classNames) : (itemClass.classNames || '')
+      cellRefs.forEach(function(ref) {
+        itemsMap[ref] = {
+          id       : 'item-' + moment(start).format('YYYYMMDDHHmmss') + '-' + moment(end).format('YYYYMMDDHHmmss'),
+          name     : item.name,
+          classes  : (itemsMap[ref]) ? (itemsMap[ref].classes + ' ' + item.classes) : (item.classes || ''),
+          cellRefs : cellRefs,
+          firstRef : cellRefs[0],
+          lastRef  : cellRefs[cellRefs.length-1]
         }
-        defaultStyles[key] = DEFAULT_ITEM_OVERLAY_STYLE;
       });
-      this.setState({itemOverlayStyles: defaultStyles});
     }, this);
-    return itemClassesMap;
+
+    return itemsMap;
   },
 
   getHeaderColumns: function() {
     var cols = [];
     for (var i = 0; i < this.props.numberOfDays; i++) {
-      cols.push(moment(this.state.date).add(i, 'days'));
+      cols.push(moment(this.state.date).add(i, 'days').toDate());
     }
     return cols;
   },
@@ -138,19 +141,18 @@ var HvReactAgenda = React.createClass({
   getMinuteCells: function(rowMoment) {
     var cells = [];
     for (var i = 0; i < this.props.numberOfDays; i++) {
-      var cellKey = moment(rowMoment).add(i, 'days').format('YYYY-MM-DDTHH:mm:ss');
+      var cellRef = moment(rowMoment).add(i, 'days').format('YYYY-MM-DDTHH:mm:ss');
       cells.push({
-        dayIndex: i,
-        cellKey: cellKey,
-        item: this.state.itemClasses[cellKey] || DEFAULT_ITEM
+        cellRef: cellRef,
+        item: this.state.items[cellRef] || DEFAULT_ITEM
       });
     }
     return cells;
   },
 
-  getItemOverlayStyle: function(cellKey) {
-    if (this.state.itemOverlayStyles[cellKey]) {
-      return this.state.itemOverlayStyles[cellKey];
+  getItemOverlayStyle: function(itemId) {
+    if (this.state.itemOverlayStyles[itemId]) {
+      return this.state.itemOverlayStyles[itemId];
     } else {
       return {
         display: 'none'
@@ -159,40 +161,48 @@ var HvReactAgenda = React.createClass({
   },
 
   handleMouseEnter: function(cell) {
-    if (cell.item.itemKeys.length) {
+    if (cell.item.id) {
+      this.setState({hoveredItemId: cell.item.id});
+    }
+      //if (cell.item.cellRefs.length) {
+      //  cell.item.itemKeys.forEach(function(key, i) {
+      //    var node = this.refs[key].getDOMNode();
+      //    addDomClass(node, '--hovered');
+      //    if (i === 0) {
+      //      addDomClass(node, '--hovered-first');
+      //    } else if (i === cell.item.itemKeys.length-1) {
+      //      addDomClass(node, '--hovered-last');
+      //    }
+      //  }, this);
+      //}
+
+      /*
       var firstHovered;
       var lastHovered;
 
-      cell.item.itemKeys.forEach(function(key, i) {
-        var node = this.refs[key].getDOMNode();
-        addDomClass(node, '--hovered');
-        if (i === 0) {
-          firstHovered = node;
-          addDomClass(node, '--hovered-first');
-        } else if (i === cell.item.itemKeys.length-1) {
-          lastHovered = node;
-          addDomClass(node, '--hovered-last');
-        }
-      }, this);
+      firstHovered = node;
+      lastHovered = node;
 
-      var newStyles = {};
-      newStyles[cell.cellKey] = {
-        display: 'block',
-        position: 'absolute',
-        width: firstHovered.offsetWidth,
-        textAlign: 'center',
-        height: 0,
-        lineHeight: 0,
-        overflow: 'visible',
-        border: 'none',
-        top: firstHovered.getBoundingClientRect().top + ((lastHovered.getBoundingClientRect().bottom - firstHovered.getBoundingClientRect().top)/2)
+      var overlayStyles = this.state.itemOverlayStyles;
+      overlayStyles[cell.cellKey] = {
+        display    : 'block',
+        position   : 'absolute',
+        width      : firstHovered.offsetWidth,
+        textAlign  : 'center',
+        height     : 0,
+        lineHeight : 0,
+        overflow   : 'visible',
+        border     : 'none',
+        top        : firstHovered.getBoundingClientRect().top + ((lastHovered.getBoundingClientRect().bottom - firstHovered.getBoundingClientRect().top)/2)
       };
 
-      this.setState({itemOverlayStyles: newStyles});
-    }
+      this.setState({itemOverlayStyles: overlayStyles});
+      */
   },
 
   handleMouseLeave: function(cell) {
+    this.setState({hoveredItemId: -1});
+    /*
     if (cell.item.itemKeys.length) {
       cell.item.itemKeys.forEach(function(key, i) {
         var node = this.refs[key].getDOMNode();
@@ -201,22 +211,20 @@ var HvReactAgenda = React.createClass({
         removeDomClass(node, '--hovered');
       }, this);
 
-      var newStyles = {};
-      newStyles[cell.cellKey] = {
-        display: 'none'
-      };
+      var overlayStyles = this.state.itemOverlayStyles;
+      overlayStyles[cell.cellKey].display = 'none';
 
-      this.setState({itemOverlayStyles: newStyles});
+      this.setState({itemOverlayStyles: overlayStyles});
     }
+    */
   },
 
   render: function() {
 
     var renderHeaderColumns = function(col, i) {
       return (
-        <th className="agenda__cell --head">
-          {col.format('ddd M\/D')}
-          <div>{col.format('ddd M\/D')}</div>
+        <th key={"col-" + i} className="agenda__cell --head">
+          {moment(col).format('ddd M\/D')}
         </th>
       );
     };
@@ -225,14 +233,14 @@ var HvReactAgenda = React.createClass({
       if (i % this.props.rowsPerHour === 0) {
         var ref = "hour-" + Math.floor(i/this.props.rowsPerHour);
         return (
-          <tr ref={ref} className="agenda__row --hour-start">
+          <tr key={"row-" + i} ref={ref} className="agenda__row --hour-start">
             <td className="agenda__cell --time" rowSpan={this.props.rowsPerHour}>{row.format('LT')}</td>
             {this.getMinuteCells(row).map(renderMinuteCells, this)}
           </tr>
         );
       } else {
         return (
-          <tr>
+          <tr key={"row-" + i}>
             {this.getMinuteCells(row).map(renderMinuteCells, this)}
           </tr>
         );
@@ -240,36 +248,45 @@ var HvReactAgenda = React.createClass({
     };
 
     var renderMinuteCells = function(cell, i) {
+      var cellClasses = {
+        'agenda__cell'      : true,
+        '--hovered'         : (this.state.hoveredItemId === cell.item.id),
+        '--hovered-first'   : (this.state.hoveredItemId === cell.item.id) && (cell.item.firstRef === cell.cellRef),
+        '--hovered-last'    : (this.state.hoveredItemId === cell.item.id) && (cell.item.lastRef  === cell.cellRef)
+      };
+      cellClasses[cell.item.classes] = true;
+
+      var classSet = React.addons.classSet(cellClasses);
+
       return (
         <td
-          ref={cell.cellKey}
+          ref={cell.cellRef}
+          key={"cell-" + i}
           onMouseEnter={this.handleMouseEnter.bind(this, cell)}
           onMouseLeave={this.handleMouseLeave.bind(this, cell)}
-          className={"agenda__cell " + cell.item.itemClasses}
+          className={classSet}
         >
-          <div style={this.getItemOverlayStyle(cell.cellKey)} className="agenda__item-overlay-title">{cell.item.itemName}</div>
+          <div style={this.getItemOverlayStyle(cell.item.id)} className="agenda__item-overlay-title">{cell.item.name}</div>
         </td>
       );
     };
 
     return (
       <div className="agenda">
-        <div ref="scrollContainer" className="agenda__frame">
-          <table>
-            <thead>
-              <tr>
-                <th className="agenda__cell --controls">
-                  <div className="agenda__prev" onClick={this.prevRange}></div>
-                  <div className="agenda__next" onClick={this.nextRange}></div>
-                </th>
-                {this.getHeaderColumns().map(renderHeaderColumns)}
-              </tr>
-            </thead>
-            <tbody>
-              {this.getBodyRows().map(renderBodyRows, this)}
-            </tbody>
-          </table>
-        </div>
+        <table>
+          <thead>
+            <tr>
+              <th className="agenda__cell --controls">
+                <div className="agenda__prev" onClick={this.prevRange}><span>&laquo;</span></div>
+                <div className="agenda__next" onClick={this.nextRange}><span>&raquo;</span></div>
+              </th>
+              {this.getHeaderColumns().map(renderHeaderColumns)}
+            </tr>
+          </thead>
+          <tbody ref="scrollContainer">
+            {this.getBodyRows().map(renderBodyRows, this)}
+          </tbody>
+        </table>
       </div>
     );
   }
